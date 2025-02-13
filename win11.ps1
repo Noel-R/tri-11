090d8cf9b6542ead934e5427650d98115cc8d105
Noel Rottenbiller <73253924+Noel-R@users.noreply.github.com>
2025-02-13 14:34:53 +0000

    Update win11.ps1
    Removed redundant code, fixed restart after finish.

--- win11.ps1
+++ win11.ps1
@@ -1,59 +1,9 @@
 #region Initialization
-function Write-DarkGrayDate {
-    [CmdletBinding()]
-    param (
-        [Parameter(Position = 0)]
-        [System.String]
-        $Message
-    )
-    if ($Message) {
-        Write-Host -ForegroundColor DarkGray "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) $Message"
-    }
-    else {
-        Write-Host -ForegroundColor DarkGray "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) " -NoNewline
-    }
-}
-function Write-DarkGrayHost {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true, Position = 0)]
-        [System.String]
-        $Message
-    )
-    Write-Host -ForegroundColor DarkGray $Message
-}
-function Write-DarkGrayLine {
-    [CmdletBinding()]
-    param ()
-    Write-Host -ForegroundColor DarkGray '========================================================================='
-}
-function Write-SectionHeader {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true, Position = 0)]
-        [System.String]
-        $Message
-    )
-    Write-DarkGrayLine
-    Write-DarkGrayDate
-    Write-Host -ForegroundColor Cyan $Message
-}
-function Write-SectionSuccess {
-    [CmdletBinding()]
-    param (
-        [Parameter(Position = 0)]
-        [System.String]
-        $Message = 'Success!'
-    )
-    Write-DarkGrayDate
-    Write-Host -ForegroundColor Green $Message
-}
+
 #endregion
 
 $ScriptName = 'Triumph Windows 11 Config'
 $ScriptVersion = '1'
-Write-Host -ForegroundColor Green "$ScriptName $ScriptVersion"
-
 
 #Variables to define the Windows OS / Edition etc to be applied during OSDCloud
 $Product = (Get-MyComputerProduct)
@@ -69,21 +19,18 @@ $OSLanguage = 'en-gb'
 
 #Set OSDCloud Vars
 $Global:MyOSDCloud = [ordered]@{
-    Restart = [bool]$False
+    Restart = [bool]$true
     RecoveryPartition = [bool]$true
-    OEMActivation = [bool]$True
+    OEMActivation = [bool]$true
     WindowsUpdate = [bool]$true
     WindowsUpdateDrivers = [bool]$true
     WindowsDefenderUpdate = [bool]$true
     SetTimeZone = [bool]$true
-    ClearDiskConfirm = [bool]$False
+    ClearDiskConfirm = [bool]$false
     ShutdownSetupComplete = [bool]$false
     SyncMSUpCatDriverUSB = [bool]$true
     CheckSHA1 = [bool]$true
 }
-
-#Testing MS Update Catalog Driver Sync
-#$Global:MyOSDCloud.DriverPackName = 'Microsoft Update Catalog'
 
 #Used to Determine Driver Pack
 $DriverPack = Get-OSDCloudDriverPack -Product $Product -OSVersion $OSVersion -OSReleaseID $OSReleaseID
@@ -94,7 +41,6 @@ if ($DriverPack){
 
 
 #write variables to console
-Write-SectionHeader "OSDCloud Variables"
 Write-Output $Global:MyOSDCloud
 
 #Update Files in Module that have been updated since last PowerShell Gallery Build (Testing Only)
@@ -102,20 +48,6 @@ $ModulePath = (Get-ChildItem -Path "$($Env:ProgramFiles)\WindowsPowerShell\Modul
 import-module "$ModulePath\OSD.psd1" -Force
 
 #Launch OSDCloud
-Write-SectionHeader -Message "Starting OSDCloud"
 write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage -ZTI"
 
 Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage -ZTI
-
-Write-SectionHeader -Message "OSDCloud Process Complete, Running Custom Actions From Script Before Reboot"
-
-# Check for UploadInfo.exe on any USB drive and run it if found
-$usbDrives = Get-WmiObject -Query "SELECT * FROM Win32_Volume WHERE DriveType = 2"
-foreach ($drive in $usbDrives) {
-    $uploadInfoPath = Join-Path -Path $drive.DriveLetter -ChildPath "UploadInfo.exe"
-    if (Test-Path -Path $uploadInfoPath) {
-        Write-SectionHeader -Message "Found UploadInfo.exe on $($drive.DriveLetter), executing it"
-        & $uploadInfoPath
-        break
-    }
-}
